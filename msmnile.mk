ce0a0bc70f8178ee6a409d1c68ddec64b57abfa0
me-cafebabe <me.cafebabe@gmail.com>
2025-08-21 12:57:24 +0500

    sm8150-common: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- msmnile.mk
+++ msmnile.mk
@@ -318,7 +318,8 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.sh \
     init.qti.chg_policy.sh \
-    init.qti.dcvs.sh
+    init.qti.dcvs.sh \
+    usb_compositions.conf
 
 PRODUCT_PACKAGES += \
     init.qcom.power.rc \
@@ -356,7 +357,8 @@ PRODUCT_BOOT_JARS += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
