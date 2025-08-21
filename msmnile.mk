b9c2cd72eac0dee5c3900d7940144f0be9cb60b3
Michael Bestas <mkbestas@lineageos.org>
2025-08-21 12:53:37 +0500

    sm8150-common: Switch to AIDL USB HAL
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624

--- msmnile.mk
+++ msmnile.mk
@@ -356,7 +356,7 @@ PRODUCT_BOOT_JARS += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb-service.qti
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
