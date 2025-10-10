21c769ec7e06a987a06273e011a43fa5657e834c
duckyduckG <duckyduckg65@gmail.com>
2025-10-11 01:46:22 +0500

    sm8150-common: use common qcom usb scripts
    AIDL USB HAL requires common qcom usb scripts. current usb scripts
    in device tree are old and for dual-role usb hal for which support
    is dropped in kernel 4.19
    
    Change-Id: I65b9f218f57627795a4239cada7e1f5c569878f6

--- msmnile.mk
+++ msmnile.mk
@@ -295,7 +295,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
-    hardware/qcom-caf/common/libqti-perfd-client
+    hardware/qcom-caf/common/libqti-perfd-client \
+    vendor/qcom/opensource/usb/etc
 
 # Public libraries
 PRODUCT_COPY_FILES += \
