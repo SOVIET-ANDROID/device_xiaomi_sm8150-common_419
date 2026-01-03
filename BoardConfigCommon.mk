e01c55b474f1df55ae1608cb3b260a118671202b
kondors1995 <normandija1945@gmail.com>
2026-01-03 19:54:11 +0000

    Revert "sm8150-common: Build AIDL NXP NFC HAL"
    This reverts commit 384d3ddf48e7e4cf6f3f3a6e448cb49b8a1c8c29.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -235,7 +235,9 @@ endif
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml
+
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 ifeq ($(TARGET_HAS_FM),true)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_fm.xml
